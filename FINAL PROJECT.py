a7cb182e448e52265d92e55019b5d1839b665c72
shatoum <sarahwhatoum@gmail.com>
2015-11-03 14:31:23 -0500

    final project - metadata
    printed metadata I wanted

--- FINAL PROJECT.py
+++ FINAL PROJECT.py
@@ -1,44 +1,39 @@
 import requests
 import json
 
-#create lists for the metadata I want:
 
-pk = []
-primary_image_id = []
-longitude = []
-latitude = []
-place = []
-title = []
-date_text = []
-#And I only want photographs, so:
-object_type = []
+               #range = (start, stop, offset)
+offsetvalues = range(0, 129, 45)
+response_dict = {}
 
-payload = { 'q' : "Linnaeus+Tripe", 'limit' : 45, 'offset' : 3 }
-r = requests.get("http://www.vam.ac.uk/api/json/museumobject/", params=payload)
+#since we only want images, want the maximum amount of records (45), and need to see all records, add to payload
+for offsetval in offsetvalues:
+    payload = { 'q' : "Linnaeus+Tripe", 'images' : 1, 'limit' : 45, 'offset' : offsetval }
+    r = requests.get("http://www.vam.ac.uk/api/json/museumobject/", params=payload)
+    response_dict = json.loads(r.text)
+    for a_record in response_dict['records']:
+        row = [a_record['pk'],a_record['fields']['primary_image_id'],a_record['fields']['longitude'],a_record['fields']['latitude'],a_record['fields']['place'],
+            a_record['fields']['title'],a_record['fields']['date_text']]
 
-response_dict = json.loads(r.text)
+        print(row)    
 
-for a_record in response_dict['records']:
-    try:
-        print(a_record)
-    except:
-        print("No record.")
-
-for a_title in response_dict['records']['fields'][15]:
-    try:
-        print(a_title)
-    except:
-        print("None.")
-
-
-
-
-        
-#write to csv:
-
-
-
-              
+##response_dict = json.loads(r.text)
+##
+##for a_record in response_dict['records']:
+##    try:
+##        print(a_record)
+##    except:
+##        print("No record.")
+##
+##for a_title in response_dict['records']['fields'][15]:
+##    try:
+##        print(a_title)
+##    except:
+##        print("None.")
+##
+##
+##
+##             
 
 ##for a_record in response_dict['records']['fields']:
 ##    print(response_dict)
