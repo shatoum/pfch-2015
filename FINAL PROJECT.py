19d1f397dba476b9255901f85ec50d282262167c
shatoum <sarahwhatoum@gmail.com>
2015-12-13 12:53:17 -0500

    Final version of final project

--- FINAL PROJECT.py
+++ FINAL PROJECT.py
@@ -1,48 +1,37 @@
 import requests
 import json
+import re
+import csv
+
+with open("finalproject.csv","w") as a_file:
+
+    offsetvalues = range(0, 129, 45)
+    response_dict = {}
+
+    for offsetval in offsetvalues:
+        payload = { 'q' : "Linnaeus+Tripe", 'images' : 1, 'limit' : 45, 'offset' : offsetval }
+        r = requests.get("http://www.vam.ac.uk/api/json/museumobject/", params=payload)
+        response_dict = json.loads(r.text)
+        for a_record in response_dict['records']:
+                    
+            row = [a_record['pk'], a_record['fields']['primary_image_id'],a_record['fields']['latitude'],a_record['fields']['longitude'],a_record['fields']['place'],
+                a_record['fields']['title'],a_record['fields']['date_text']]
+            row = [str(x).replace("\n", "") for x in row]
+            a_file.write(','.join(row)+'\n')
 
 
-               #range = (start, stop, offset)
-offsetvalues = range(0, 129, 45)
-response_dict = {}
-
-#since we only want images, want the maximum amount of records (45), and need to see all records, add to payload
-for offsetval in offsetvalues:
-    payload = { 'q' : "Linnaeus+Tripe", 'images' : 1, 'limit' : 45, 'offset' : offsetval }
-    r = requests.get("http://www.vam.ac.uk/api/json/museumobject/", params=payload)
-    response_dict = json.loads(r.text)
-    for a_record in response_dict['records']:
-        row = [a_record['pk'],a_record['fields']['primary_image_id'],a_record['fields']['longitude'],a_record['fields']['latitude'],a_record['fields']['place'],
-            a_record['fields']['title'],a_record['fields']['date_text']]
-
-        print(row)    
-
-##response_dict = json.loads(r.text)
-##
-##for a_record in response_dict['records']:
-##    try:
-##        print(a_record)
-##    except:
-##        print("No record.")
-##
-##for a_title in response_dict['records']['fields'][15]:
-##    try:
-##        print(a_title)
-##    except:
-##        print("None.")
-##
-##
-##
-##             
-
-##for a_record in response_dict['records']['fields']:
-##    print(response_dict)
-##    try:
-##        #lets store the name in its own variable
-##	#the [0] is because it is an list, so we only want the first author
-##        year_start = a_record['records']['fields']
-##        print("Looking at",year_start)
-##    except:
-##        print("No year.")
+           
+        
+        #our_text = a_record['fields']['primary_image_id']
+        #compile  pattern with regex
+        #p = re.compile('^[0-9]{4}..')
+        #m = p.findall(our_text)
+        
 
 
+
+
+
+
+
+        
